e39adbc4df73403c8fc018cc16add99aaa4c6289
SonaliMahendra <sonalimahendran6@gmail.com>
2022-09-21 05:09:31 +0530

    Stack-Brackets Balancing

---
+++ src/com/ivycomptech/Stack/Brackets.java
@@ -0,0 +1,30 @@
+package com.ivycomptech.Stack;
+import java.util.*;
+public class Brackets {
+    public static void main(String []argh)
+    {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNext()) {
+            String input= sc.next();
+            System.out.println(checkIfBalanced(input,'{', '}')&&checkIfBalanced(input,'[', ']')&&checkIfBalanced(input,'(', ')'));
+        }
+    }
+
+    public static boolean checkIfBalanced(String input, char opening, char closing){
+        Stack<String> stack = new Stack<>();
+        for(int i = 0; i < input.length();i++){
+            char c = input.charAt(i);
+            if(c == closing){
+                if(!stack.empty()){
+                    stack.pop();
+                }else{
+                    return false;
+                }
+            }
+            if(c == opening){
+                stack.push(String.valueOf(c));
+            }
+        }
+        return stack.empty();
+    }
+}
