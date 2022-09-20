d6e75469c078d7033cfa20fa12552fb0e9694774
SonaliMahendra <sonalimahendran6@gmail.com>
2022-09-21 00:34:56 +0530

    Even Numbers

---
+++ src/com/ivycomptech/EvenNumbers/Even.java
@@ -0,0 +1,12 @@
+package com.ivycomptech.EvenNumbers;
+
+public class Even {
+    public static void main(String[] args) {
+        int number = 50;
+        System.out.print("List of even numbers from 1 to " + number + ": ");
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0)
+                System.out.print(i + " ");
+        }
+    }
+}
