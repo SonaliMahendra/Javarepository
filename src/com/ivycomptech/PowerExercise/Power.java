9434de702ad290fbe162e34e3dd1d60b65c6ba6a
SonaliMahendra <sonalimahendran6@gmail.com>
2022-09-21 01:05:10 +0530

    Power

--- src/com/ivycomptech/PowerExercise/Power.java
+++ src/com/ivycomptech/PowerExercise/Power.java
@@ -1,11 +1,16 @@
 package com.ivycomptech.PowerExercise;
 
+import java.util.Scanner;
+
 public class Power {
     public static void main(String[] args) {
-        int num = 3, exponent = 4;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter base and exponent:");
+        int base = s.nextInt();
+        int exponent = s.nextInt();
         long result = 1;
         while (exponent != 0) {
-            result *= num;
+            result *= base;
             --exponent;
         }
         System.out.println("Answer = " + result);
