eb1cc63620fc5fb686cd5b6461d25cea37cb1c40
SonaliMahendra <sonalimahendran6@gmail.com>
2022-09-21 00:37:48 +0530

    Character at given index

---
+++ src/com/ivycomptech/StringFunction/CharAtGivenIndex.java
@@ -0,0 +1,9 @@
+package com.ivycomptech.StringFunction;
+
+public class CharAtGivenIndex {
+    public static void main(String[] args) {
+        String name="Java Exercises";
+        char ch=name.charAt(5);
+        System.out.println(ch);
+    }
+}
