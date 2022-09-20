2ed85273dd0a0941a34f0d60127cb594a9fb4e9b
SonaliMahendra <sonalimahendran6@gmail.com>
2022-09-21 00:37:08 +0530

    Cutters

---
+++ src/com/ivycomptech/CuttersExercise/Cutters.java
@@ -0,0 +1,9 @@
+package com.ivycomptech.CuttersExercise;
+
+public class Cutters {
+    public static void main(String[] args) {
+        String[] elements= {"nail cutter","cookie cutter","cable cutter","vegetable cutter","micro cutter"};
+        for (String s: elements)
+            System.out.println(s);
+    }
+}
