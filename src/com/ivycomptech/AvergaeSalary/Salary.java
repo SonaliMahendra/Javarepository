1f2d8a82d80738b06e439e47dfd1d72cce8b7a1f
SonaliMahendra <sonalimahendran6@gmail.com>
2022-09-21 05:22:52 +0530

    Salary

---
+++ src/com/ivycomptech/AvergaeSalary/Salary.java
@@ -0,0 +1,24 @@
+package com.ivycomptech.AvergaeSalary;
+import java.util.Arrays;
+public class Salary {
+    public static double average(int[] salary) {
+        int n=salary.length;
+        int mn=Integer.MAX_VALUE,mx=Integer.MIN_VALUE,sum=0;
+        for(int i:salary)
+        {
+            sum=sum+salary[i];
+            mn=Math.min(mn,salary[i]);
+            mx=Math.max(mx,salary[i]);
+        }
+        double ans=(sum-mn-mx)/((n-2)*1.0);
+        if(n>2)
+            return ans;
+        else
+            return 0;
+    }
+    public static void main(String[] args) {
+        int [] arr = {4000,3000,1000,2000};
+        double ans=  average(arr);
+        System.out.println(ans);
+    }
+}
