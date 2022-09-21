5b3875e70c51403691270bbc0bebd4a86481e2e6
SonaliMahendra <sonalimahendran6@gmail.com>
2022-09-21 17:52:00 +0530

    Java Collections Ex4

--- src/com/ivycomptech/RetreiveElement/Retrieve.java
+++ src/com/ivycomptech/RetreiveElement/Retrieve.java
@@ -3,16 +3,16 @@ package com.ivycomptech.RetreiveElement;
 import java.util.*;
 public class Retrieve {
     public static void main(String[] args) {
-        List<String> list_Strings = new ArrayList<>();
-        list_Strings.add("Red");
-        list_Strings.add("Green");
-        list_Strings.add("Orange");
-        list_Strings.add("White");
-        list_Strings.add("Black");
-        System.out.println(list_Strings);
-        String element = list_Strings.get(0);
+        List<String> listStrings = new ArrayList<>();
+        listStrings.add("Red");
+        listStrings.add("Green");
+        listStrings.add("Orange");
+        listStrings.add("White");
+        listStrings.add("Black");
+        System.out.println(listStrings);
+        String element = listStrings.get(0);
         System.out.println("First element: " + element);
-        element = list_Strings.get(2);
+        element = listStrings.get(2);
         System.out.println("Third element: " + element);
     }
 }
