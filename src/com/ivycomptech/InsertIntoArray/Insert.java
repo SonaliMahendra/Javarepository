5f67d14c0257399658dcd6c088456f9577ffd015
SonaliMahendra <sonalimahendran6@gmail.com>
2022-09-21 14:06:06 +0530

    Java Collections Ex3

---
+++ src/com/ivycomptech/InsertIntoArray/Insert.java
@@ -0,0 +1,17 @@
+package com.ivycomptech.InsertIntoArray;
+
+import java.util.*;
+public class Insert {
+    public static void main(String[] args) {
+        List<String> list_Strings = new ArrayList<>();
+        list_Strings.add("Red");
+        list_Strings.add("Green");
+        list_Strings.add("Orange");
+        list_Strings.add("White");
+        list_Strings.add("Black");
+        System.out.println(list_Strings);
+        list_Strings.add(0, "Pink");
+        list_Strings.add(5, "Yellow");
+        System.out.println(list_Strings);
+    }
+}
